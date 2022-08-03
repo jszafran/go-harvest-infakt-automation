2335f41d76c6a7f9e67df468f334184a5c330366
jszafran <jszafran.pv@gmail.com>
2022-08-03 18:03:09 +0200

    Add method for Harvest Get request.

--- harvest.go
+++ harvest.go
@@ -1,17 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
 type HarvestHTTP struct {
 	Config HarvestConfig
+	client http.Client
 }
 
-func (h HarvestHTTP) NewHarvestClient(configPath string) (HarvestHTTP, error) {
+func NewHarvestClient(configPath string) (HarvestHTTP, error) {
 	var hv HarvestHTTP
 	hvConf, err := HarvestConfigFromJSON(configPath)
 	if err != nil {
 		return hv, err
 	}
 
+	httpClient := http.Client{Timeout: time.Second * 5}
 	return HarvestHTTP{
 		Config: hvConf,
+		client: httpClient,
 	}, nil
+}
+
+func (h HarvestHTTP) getRequest(path string) (*http.Response, error) {
+	if string(path[0]) == "/" {
+		path = path[1:]
+	}
+
+	url := fmt.Sprintf("%s%s", h.Config.HarvestApiUrl, path)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatalf("Failed to create requqest: %v", err)
+	}
+
+	accId := strconv.Itoa(h.Config.HarvestAccountId)
+	req.Header.Set("Harvest-Account-ID", accId)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", h.Config.HarvestAuthToken))
+
+	return h.client.Do(req)
 }
