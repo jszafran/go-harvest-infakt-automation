e68caf94a96e186385740b85b7757833c4cd226f
jszafran <jszafran.pv@gmail.com>
2022-08-03 17:49:03 +0200

    Add factory functions for Infakt/Harvest clients.

--- harvest.go
+++ harvest.go
@@ -3,3 +3,15 @@ package main
 type HarvestHTTP struct {
 	Config HarvestConfig
 }
+
+func (h HarvestHTTP) NewHarvestClient(configPath string) (HarvestHTTP, error) {
+	var hv HarvestHTTP
+	hvConf, err := HarvestConfigFromJSON(configPath)
+	if err != nil {
+		return hv, err
+	}
+
+	return HarvestHTTP{
+		Config: hvConf,
+	}, nil
+}
--- infakt.go
+++ infakt.go
@@ -3,3 +3,15 @@ package main
 type InfaktHTTP struct {
 	Config InfaktConfig
 }
+
+func (i InfaktHTTP) NewInfaktClient(configPath string) (InfaktHTTP, error) {
+	var ifkt InfaktHTTP
+	ifConf, err := InfaktConfigFromJSON(configPath)
+	if err != nil {
+		return ifkt, err
+	}
+
+	return InfaktHTTP{
+		Config: ifConf,
+	}, nil
+}
