3bc59b21a7cce90651d6488dd49e77e1e75f3863
jszafran <jszafran.pv@gmail.com>
2022-08-04 14:30:38 +0200

    Initial work on data structures for Infakt requests.

--- infakt.go
+++ infakt.go
@@ -4,6 +4,17 @@ type InfaktHTTP struct {
 	Config InfaktConfig
 }
 
+type InvoiceDetails struct {
+	ClientId       string `json:"client_id"`
+	PaymentMethods string `json:"payment_methods"`
+	SalesDate      string `json:"sales_date"`
+	InvoiceDate    string `json:"invoice_date"`
+}
+
+type DraftInvoiceRequest struct {
+	Invoice InvoiceDetails `json:"invoice"`
+}
+
 func (i InfaktHTTP) NewInfaktClient(configPath string) (InfaktHTTP, error) {
 	var infakt InfaktHTTP
 	infaktConf, err := InfaktConfigFromJSON(configPath)
